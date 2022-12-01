cf0f4de9ed8674673a63288260f3acec7cf462bd
Sabine.Eitel <Seit201@aol.com>
2022-12-01 10:10:08 +0100

    Klausur

--- funktionen.js
+++ funktionen.js
@@ -6,31 +6,122 @@
 
 // Wertzuweisung / Assignment
 
+// a = 2; // number
+// a = true; // boolean (true, false)
+// a = "Hallo"; // string
+
+
+// console.log(typeof a);
+// console.log(a);
 
 /* Arrays */
 // Navigation über INDEX
 
+// let arr; 
+// arr = new Array();   // Möglichkeit
+// arr = [false,true,true,false];
 
-
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// console.log(arr.length);
+// console.log(arr[arr.length-1]);
+// console.log(arr[5]);
 
 /* Array -Info, nicht klausurrelevant*/
-
+// let arr = [];
+// console.log(arr);
+// arr.push("a");
+// console.log(arr);
+// arr.push("b");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 /* Daten-Objekte */
 // Navigation über Bezeichner (keys)
 
+// let obj = {x:"Hi", y:29};                    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// console.log(obj);
+// console.log(obj.x);
+// console.log(obj.y);
+
+// let obj = {
+//         val0: 10,
+//         sub1:{
+//             val1: "hi",
+//             val2: 2
+//             }
+//     }
+// console.log(obj);
+// console.log(obj.val0);
+// console.log(obj.sub1.val2);
+// console.log(obj.sub1.val1);    // Datenobjekt
 
 
- /*DOM*/
+// {{{{{{{{{}}}}}}}}}    //   nested object!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ //*DOM*--> Modell der HTML Struktur
+ // DOM --> Document Object Modell
+ // DOM --> nach dem Parsen / Laden
+
+
+// console.log(document);
+// console.log(document.body);
+// console.log(document.body.children);
+// console.log(document.body.children[0]);
+// console.log(document.body.children[0].innerHTML);
+// document.body.children[0].innerText = "Hi";
+// console.log(document.getElementById("test"));
+// document.getElementById("test").innerHTML = "Hallo Text!"
+
+
 
 
 /* ENTSCHEIDUNGSSTRUKTUREN | CONTROL FLOW*/
 
+// let cond;
 
+// cond = true;
+// cond = (6 > 4);
+// cond = (6 < 4);
+// cond = (4 == 4); // Test auf Wert             //   = numerisch, == wert und typ gleich
+// cond = (4 === "4"); // Test auf Wert UND Typ
+
+
+
+// if (cond) {
+//     console.log("Aussage wahr");
+// } else {
+//     console.log("Aussage falsch");
+// }
+
+// switch (key) {                        // Klausur relevant !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
 
  //   https://github.com/01-PP/03_Entscheidungsstrukturen
 
 /* FUNKTIONEN */
+
+// 1. Kapselung
+test();  // call
+function test() { // Fkt.-Rumpf | body
+    console.log("Testausgabe");
+}
+
+
+
+
+
+
 
 
 // 3. Parameter 
