df3680912fa4a1ebef2c3ca282d838df63b83757
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-12-01 09:13:13 +0200

    Add files via upload

---
+++ funktionen.js
@@ -0,0 +1,60 @@
+/* DATENSTRUKTUREN */
+
+/* Primitives */
+
+// let a // (var) Definition oder Deklaration
+
+// Wertzuweisung / Assignment
+
+
+/* Arrays */
+// Navigation über INDEX
+
+
+
+
+/* Array -Info, nicht klausurrelevant*/
+
+
+/* Daten-Objekte */
+// Navigation über Bezeichner (keys)
+
+
+
+ /*DOM*/
+
+
+/* ENTSCHEIDUNGSSTRUKTUREN | CONTROL FLOW*/
+
+
+
+ //   https://github.com/01-PP/03_Entscheidungsstrukturen
+
+/* FUNKTIONEN */
+
+
+// 3. Parameter 
+//  console.log(test3("Robert")); // Argument(e)
+//  function test3(firstName) // Parameter
+//  { // do something ...
+//     return "Hallo, " + firstName; 
+// }
+
+// 4. Fkt-Call aus Funktionen
+// console.log(test4("Robert")); // Argument(e)
+
+// function test4(firstName) // Parameter
+// { 
+//    return grussformel() + firstName; 
+//    // return firstName + grussformel() ; 
+// }
+
+// function grussformel() {
+//    // return "Hallo, ";
+//     return "Hola, ";
+// }
+
+// rechner();
+
+
+
